fb8ca6a2cb478fd60fb1e2d6af983c3a4d74593a
NIEN <trinhnien251@gmail.com>
2024-11-11 13:53:30 +0700

    create orders modal

--- Model/admin/Orders.php
+++ Model/admin/Orders.php
@@ -1,3 +1,45 @@
+<?php
 class Orders {
-    
+    public $connect;
+
+    public function __construct()
+    {
+        $this->connect = new ConnectDatabase();
+    }
+
+    public function insertOrders($id, $id_users, $status, $methods_payment, $another_address, $create_at){
+        $sql="INSERT INTO `orders`(`id`, `id_users`, `status`, `methods_payment`, `another_address`, `create_at`) VALUES ('?','?','?','?','?','?')";
+        $this->connect->setQuery($sql);
+        $check = $this->connect->loadData([$id, $id_users, $status, $methods_payment, $another_address, $create_at]);
+        if($check){
+            return true;
+        }
+    }
+
+    public function getAllOrders(){
+        $sql="SELECT * FROM `orders` ";
+        $this->connect->setQuery($sql);
+        return $this->connect->loadData();
+    }
+
+    public function getOrdersById($id){
+        $sql="SELECT * FROM `orders` where id = ? ";
+        $this->connect->setQuery($sql);
+        return $this->connect->loadData([$id],false);
+    }
+
+    public function updateOrders($id,$status,$methods_payment,$another_address,$create_at){
+        $sql="UPDATE `orders` SET ,`status`='?',`methods_payment`='?',`another_address`='?',`create_at`='?' WHERE `id`='$id'";
+        $this->connect->setQuery($sql);
+        $check = $this->connect->loadData([$id,$status,$methods_payment,$another_address,$create_at]);
+        if($check){
+            return true;
+        }
+    }
+
+    public function deleteOrders($id){
+        $sql="DELETE FROM `orders` id=$id";
+        $this->connect->setQuery($sql);
+        return $this->connect->execute([$id]);
+    }
 }
