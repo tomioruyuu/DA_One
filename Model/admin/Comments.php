87591d808ae0e5e58851250834812a26bc20086e
NIEN <trinhnien251@gmail.com>
2024-11-13 12:39:12 +0700

    Tao model comments

--- Model/admin/Comments.php
+++ Model/admin/Comments.php
@@ -7,5 +7,32 @@
         {
             $this->connect = new ConnectDatabase();
         }
+       
+        public function getAllComments(){
+            $sql="SELECT * FROM `comments` ";
+            $this->connect->setQuery($sql);
+            return $this->connect->loadData();
+        }
+    
+        public function getCommentsById($id){
+            $sql="SELECT * FROM `comments` where id = ? ";
+            $this->connect->setQuery($sql);
+            return $this->connect->loadData([$id],false);
+        }
+    
+        public function updateComments($content){
+            $sql="UPDATE `comments` SET content`='?'";
+            $this->connect->setQuery($sql);
+            $check = $this->connect->loadData([$content]);
+            if($check){
+                return true;
+            }
+        }
+    
+        public function deleteComments($id){
+            $sql="DELETE FROM `comments` id=$id";
+            $this->connect->setQuery($sql);
+            return $this->connect->execute([$id]);
+        }
     }
 ?>
