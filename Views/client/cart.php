04f66454f0c879f5cebfc6c63612bdf1b5a8d240
Tuanlak05 <Tuanlak05@gmail.com>
2024-11-22 20:49:40 +0700

    update cart

---
+++ Views/client/cart.php
@@ -0,0 +1,48 @@
+
+<div class="main_content">
+    <div class="main_content_left"> 
+        <h2>Giỏ hàng của bạn</h2>
+        <div>
+            <p>Bạn đang có <b>1 sản phẩm</b> trong giỏ hàng</p>
+            <div>
+                <img src="" alt="Áo sơ mi" >
+                <div>
+                    <p><b>Áo sơ mi trắng nam công sở dài tay Merriman mã THMOL495</b></p>
+                    <p>37 / WHITE</p>
+                    <p>399,000₫</p>
+                    <button>-</button>
+                    <input type="text" value="1">
+                    <button>+</button>
+                    <button>Xóa</button>
+                </div>
+            </div>
+            <hr>
+                <label>Ghi chú đơn hàng</label>
+                <div class="note"></div>
+            <div>
+                <label>Xuất hóa đơn cho đơn hàng</label>
+            </div>
+        </div>
+    </div>
+
+    <div class="main_content_right">
+        <h2>Thông tin đơn hàng</h2>
+        <form>
+            <p>THỜI GIAN GIAO HÀNG</p>
+            <div>
+                <input type="radio" id="delivery-time-1" name="delivery-time">
+                <label for="delivery-time-1">Giao khi có hàng</label><br>
+                <input type="radio" id="delivery-time-2" name="delivery-time">
+                <label for="delivery-time-2">Chọn thời gian</label>
+            </div>
+            <p><b>Tổng tiền:</b> 399,000₫</p>
+            <p>
+                <ul>
+                    <li>Miễn phí vận chuyển đơn hàng từ 1,000,000₫</li>
+                    <li>Vui lòng nhập <b>MÃ_ƯU_ĐÃI</b> tại trang thanh toán</li>
+                </ul>
+            </p>
+            <button>THANH TOÁN</button>
+        </form>
+    </div>
+</div>
